4c5b55b14c6d55bd3b1a2b8e05f9333af70bc358
ruanyf <yifeng.ruan@gmail.com>
2016-12-22 22:58:01 +0800

    style: add progress indicator

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -164,11 +164,12 @@ insert_icon_image();
 (function () {
   // var pageHeight = document.documentElement.scrollHeight;
   var article = document.querySelector('.article');
-  var articleBottom = article.getBoundingClientRect().bottom;
+  var viewportHeight = document.documentElement.clientHeight;
+  var articleHeight = article.getBoundingClientRect().height;
   var prog = document.querySelector('.progress-indicator');
   window.addEventListener('scroll', function () {
     window.requestAnimationFrame(function () {
-      var perc = Math.max(0, Math.min(1, document.documentElement.scrollTop / articleBottom));
+      var perc = Math.max(0, Math.min(1, (viewportHeight - article.getBoundingClientRect().top) / articleHeight));
       updateProgress(perc);
     });
   });

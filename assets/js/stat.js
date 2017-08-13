75d0f6147baefc51609cf162ea077f949f3547dd
ruanyf <yifeng.ruan@gmail.com>
2017-08-13 09:06:46 +0800

    Updates

---
+++ assets/js/stat.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ed9afc8bc0ef42b26408c40e999ff735";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();

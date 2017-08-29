0b71de5c972f3188a890ecbfb8c348edd6c8e4b3
ruanyf <yifeng.ruan@gmail.com>
2017-08-29 14:34:14 +0800

    fix: add CNAME

--- node_modules/turpan/bin/turpan.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-var argv = require('yargs').argv;
-var md = require('../lib');
-
-var string = argv._[0];
-
-if (!string) return;
-console.log(md.render(string));
